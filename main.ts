fa7de1c949ad2c7aa9ad3395f0fa5d9aa164bbfc
MingcheL1 <m1ngch3.li@gmail.com>
2024-07-16 10:52:04 -0500

    parse file

--- main.ts
+++ main.ts
@@ -3,7 +3,14 @@ import Environment from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 import { MK_BOOL, MK_NULL, MK_NUMBER } from "./runtime/values.ts";
 lotus();
-
+async function run(filename:string){
+  const parser = new Parser();
+  const env = new Environment();
+  const input=await Deno.readTextFile(filename);
+  const program=parser.produceAST(input);
+  const result=evaluate(program,env);
+  console.log(result);
+}
 function lotus() {
   const logo = `
 \x1b[38;5;212m ██                                    
